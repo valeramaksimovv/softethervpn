4e95bcfcc01bb56b15d2096a06ceba8e278f29af
ValeraMaksimoVV <valeramaksimovv@gmail.com>
2020-11-13 14:08:23 +0200

    Add setting Firewall & swap

--- vpn_install.sh
+++ vpn_install.sh
@@ -9,7 +9,64 @@ echo "MC installed"
 
 # Add user "softether" and setting passwd
 adduser softether
+echo "user softether add"
 usermod -aG sudo softether
 
 # Reboot server
 # reboot 
+
+# SWAP setting
+echo "swap configure"
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+echo "sysctl vm.swappiness=10"
+sudo sysctl vm.swappiness=10
+echo "sysctl vm.vfs_cache_pressure=50"
+sudo sysctl vm.vfs_cache_pressure=50
+
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+
+# Install fail2ban which will block brute force attacks
+echo "install fail2ban"
+sudo apt -y install fail2ban
+echo "fail2ban installed"
+
+# Ensure that firewall is only allowing the ports that we need
+echo "add rules firewall"
+echo "Managments ports 443, 992, 1194, 5555"
+echo "vpn ports 500/udp, 1701/udp, 4500/udp, 1723/tcp"
+
+sudo ufw allow OpenSSH
+echo "ufw allow OpenSSH"
+sudo ufw allow 443
+echo "ufw allow 443"
+sudo ufw allow 992
+echo "ufw allow 992"
+sudo ufw allow 1194
+echo "ufw allow 1194"
+sudo ufw allow 5555
+echo "ufw allow 5555"
+sudo ufw allow 500/udp
+echo "ufw allow 500/udp"
+sudo ufw allow 1701/udp
+echo "ufw allow 1701/udp"
+sudo ufw allow 4500/udp
+echo "ufw allow 4500/udp"
+sudo ufw allow 1723/tcp
+echo "ufw allow 1723/tcp"
+
+# Enable the firewall
+echo "enable firewall"
+sudo ufw enable
+
+#Test it
+echo "firewall status"
+sudo ufw status
+
+
+
